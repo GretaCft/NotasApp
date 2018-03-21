6291ba67df554dc4f6389d84a09bc90be09badd8
GretaCft <gretasrv@yahoo.es>
2018-03-21 22:34:28 +0100

    maquetación basica, modelo, routing y servicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListallnotesComponent } from './listallnotes/listallnotes.component';
+import { SearchnotesComponent } from './searchnotes/searchnotes.component';
+import { ListfavnotesComponent } from './listfavnotes/listfavnotes.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AboutComponent } from './about/about.component';
+import { CreateformComponent } from './createnotes/createform/createform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    'path' : '',
+    component :  InicioComponent
+  },
+  {
+    'path' : 'about',
+    component :  AboutComponent
+  },
+  {
+    'path' : 'crear',
+    component : CreateformComponent
+  },
+  {
+    'path' : 'listarNotas',
+    component :  ListallnotesComponent
+  },
+  {
+    'path' : 'buscarNota/:titulo',
+    component :  SearchnotesComponent
+  },
+  {
+    'path' : 'listarNotasFav',
+    component :  ListfavnotesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListallnotesComponent } from './listallnotes/listallnotes.component';
+import { SearchnotesComponent } from './searchnotes/searchnotes.component';
+import { ListfavnotesComponent } from './listfavnotes/listfavnotes.component';
+import { NoteComponent } from './note/note.component';
+import { FavoriteiconComponent } from './note/favoriteicon/favoriteicon.component';
+import { CreateformComponent } from './createnotes/createform/createform.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AboutComponent } from './about/about.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListallnotesComponent,
+    SearchnotesComponent,
+    ListfavnotesComponent,
+    NoteComponent,
+    FavoriteiconComponent,
+    CreateformComponent,
+    InicioComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
